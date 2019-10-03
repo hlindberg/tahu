32a937b245f72d9246f308cf08d6619984888a94
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2019-10-03 23:26:46 +0200

    (docs) Add documentation to ArrayBuilder

--- lib/puppet/datatypes/tahu/arraybuilder.rb
+++ lib/puppet/datatypes/tahu/arraybuilder.rb
@@ -9,18 +9,28 @@ Puppet::DataTypes.create_type('Tahu::ArrayBuilder') do
     }
   PUPPET
   implementation do
+    # Adds the content of a given Array, and appends any other value.
+    # This ArrayBuilder is mutated to hold the new content and is then returned
+    # to allow chaining.
+    #
     def add(value)
       @elements ||= []
       @elements += value.is_a?(Array) ? value : [value]
       self
     end
 
+    # Appends the given value to any content in this ArrayBuilder.
+    # This ArrayBuilder is mutated to hold the new content and is then returned
+    # to allow chaining.
+    #
     def append(value)
       @elements ||= []
       @elements << value
       self
     end
 
+    # Returns a copy of the built array.
+    # This returned array is unaffected by subsequent operations on this ArrayBuilder.
     def build_array()
       @elements.dup
     end
